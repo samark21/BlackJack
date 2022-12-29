21b2006782c8c9c44daa0e22b83ad4b7cdc12ee3
samark21 <samar.kardosh@gmail.com>
2022-12-29 09:26:24 +0200

    Created Cards - (DataLoader, Card)

---
+++ src/Card.py
@@ -0,0 +1,11 @@
+from DataLoader import get_values
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = get_values(rank)
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
+
---
+++ src/DataLoader.py
@@ -0,0 +1,10 @@
+# In the mean time, the data will be set manually here
+# But I plan on reading it from a file later on. for example a .txt
+
+suits = ("Hearts", "Diamonds", "Spades", "Clubs")
+ranks = ("Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace")
+values = {"Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10,
+          "Jack": 10, "Queen": 10, "King": 10, "Ace": 11}
+
+def get_values(rank):
+    return values[rank]
